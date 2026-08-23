3463973112bb5c1dc3b30d3c459846a90401c0d4
Vandita <vanditaj008@gmail.com>
2026-08-23 16:05:03 +0530

    notebook code is done

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1 ",
+    author="Vandita",
+    author_email="vanditaj008@gmail.com",
+    install_requires=[
+        "google-generativeai",
+        "langchain",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2"
+    ],
+    packages=find_packages(),
+)
